894b6c9c526830ed551d3d9892a00f80d3cb0f63
OnkeTk <onkemetsetikane@gmail.com>
2024-03-16 14:29:19 -0700

    Rectangle #4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+
+const Rectangle = class {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let height = this.height;
+    while (height > 0) {
+      console.log('X'.repeat(this.width));
+      height--;
+    }
+  }
+
+  rotate () {
+    [this.height, this.width] = [this.width, this.height];
+  }
+
+  double () {
+    this.height *= 2;
+    this.width *= 2;
+  }
+};
+
+module.exports = Rectangle;
