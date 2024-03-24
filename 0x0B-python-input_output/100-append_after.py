722dbd62894a84cbf35df290d26852ad8f340d9f
OnkeTk <onkemetsetikane@gmail.com>
2024-03-24 12:18:15 -0700

    Search and update

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Module - 100
+- Finding a string and append text afterwards
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """Append if search string is found
+
+    Args:
+    - filename: Name of the file
+    - search_string: string to be searched
+    - new_string: The new string to append
+    """
+    text = ""
+
+    with open(filename, "r") as file:
+        for line in file:
+            text += line
+            if search_string in line:
+                text += new_string
+
+    with open(filename, "w") as file:
+        file.write(text)
