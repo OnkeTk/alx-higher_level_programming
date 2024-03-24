8bf9e675ee35c0c8235c185617ccb5ca7e06d28f
OnkeTk <onkemetsetikane@gmail.com>
2024-03-24 09:51:45 -0700

    Rectangle

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,21 +1,26 @@
 #!/usr/bin/python3
 """
-    Module that defines an empty
-    class
-
+Module - 7
+Public instance: def area
+Public instance: def integer_validator
 """
 
 
 class BaseGeometry:
-    """An empty class Base Geometry"""
+    """The BaseGeometry Class"""
 
     def area(self):
-        """public method that computes area"""
+        """Simply to raise an exception"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """validates given value"""
-        if type(value) is not int:
+        """Validates the value
+
+        Args:
+        - name (str): Name
+        - value: Value
+        """
+        if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
+        elif value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
