539452dc9c801755d4cced11e90cfbcfa165d3cf
OnkeTk <onkemetsetikane@gmail.com>
2024-03-24 05:22:24 -0700

    Divide a matrix

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,24 +1,18 @@
 #!/usr/bin/python3
 """
-Contains a function that adds two integers
+This is the "0-add_integer" module.
+The 0-add_integer module supplies one function, add_integer(a, b).
 """
 
 
-def add_integers(a, b=98):
-    """Returns the results of two integers
-
-        Args:
-        - a: First number
-        - b: Second number
-
-        Raises:
-        - TypeError: If a or b is not an integer or a float
-    """
-
-    if not isinstance(a, (int, float)):
+def add_integer(a, b):
+    """Return the addition of two numbers."""
+    if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
-
-    if not isinstance(b, (int, float)):
+    if type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
-
-    return int(a) + int(b)
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
+    return a + b
